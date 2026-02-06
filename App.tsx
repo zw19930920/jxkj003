1c2a9b84d060105fd524f958123f342fb214f3be
zw19930920 <128893753+zw19930920@users.noreply.github.com>
2026-02-06 11:45:29 +0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Navbar from './components/Navbar';
+import Navbar from '.Navbar';
 import Hero from './components/HeroSection';
 import ProductShowcase from './components/ProductShowcase';
 import TechExplodedView from './components/TechExplodedView';
